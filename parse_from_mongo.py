0030865fdc89d818d5f6e131432d51250d8aba85
sunbo13 <sunbo13@lenovo.com>
2021-01-28 16:36:11 +0800

    add query_and_parse_data1

--- parse_from_mongo.py
+++ parse_from_mongo.py
@@ -1,5 +1,6 @@
 import pymongo
 import pandas as pd
+import copy
 from datetime import datetime
 
 mongodb_url = 'mongodb://localhost:27017'
@@ -71,6 +72,60 @@ def query_and_parse_data():
 
             smart_dict[value_key].append(int(value))
             smart_dict[raw_key].append(int(raw))
+
+    # print(smart_dict)
+    return pd.DataFrame(smart_dict)
+
+
+def query_and_parse_data1():
+    db = get_mongodb()
+    smart_col = db[smart_collection]
+    smart_dataset = smart_col.find()
+    smart_dict = {
+        'date': [],
+        'serial_number': [],
+        'model': [],
+        'capacity_bytes': [],
+        'failure': []
+    }
+    # all attr ids
+    id_list = [1,3,4,5,7,8,9,10,11,12,13,100,103,170,171,172,173,174,177,184,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,210,211,212,220,231,232,235,237,241,242]
+    for id in id_list:
+        value_key = 'smart_{}_normalized'.format(id)
+        raw_key = 'smart_{}_raw'.format(id)
+        smart_dict[value_key] = []
+        smart_dict[raw_key] = []
+
+    for data in smart_dataset:
+        date = data['date'].strftime("%Y/%m/%d")
+        # print(date)
+        sn = data['disk_serial_number']
+        model = data['disk_model']
+        attrs = data['attrs']
+        smart_dict['date'].append(date)
+        smart_dict['serial_number'].append(sn)
+        smart_dict['model'].append(model)
+        smart_dict['capacity_bytes'].append(0)
+        smart_dict['failure'].append(0)
+
+        temp_id_list = copy.deepcopy(id_list)
+
+        for attr in attrs:
+            id = int(attr['Id'])
+            if id in temp_id_list:
+                temp_id_list.remove(id)
+                value_key = 'smart_{}_normalized'.format(id)
+                raw_key = 'smart_{}_raw'.format(id)
+                value = attr['Value']
+                raw = attr['RawValue'].split(' (')[0]
+                smart_dict[value_key].append(int(value))
+                smart_dict[raw_key].append(int(raw))
+
+        for id in temp_id_list:
+            value_key = 'smart_{}_normalized'.format(id)
+            raw_key = 'smart_{}_raw'.format(id)
+            smart_dict[value_key].append(0)
+            smart_dict[raw_key].append(0)
 
     # print(smart_dict)
     return pd.DataFrame(smart_dict)
